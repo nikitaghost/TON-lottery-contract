fdb2aba9c431a04e639147f89462945f2fddbd3d
nikitaghost <kitaychik2014@mail.ru>
2021-04-28 22:47:59 +0300

    add getters on Contract

--- Lottery.sol
+++ Lottery.sol
@@ -21,40 +21,57 @@ contract Lottery {
     */
 
 
-    uint256 m_ownerKey;
-    uint256 finishDate;
-    uint128 ticketCost;
-    uint8 status;
+    uint256 l_ownerKey;
+    uint256 l_finishDate;
+    uint128 l_ticketCost;
+    uint8 l_status;
+    uint8 l_percent;
 
-    uint numTickets;
+    uint l_numTickets;
     mapping(uint => Ticket) tickets;
 
-    constructor() public {
-        // Check that contract's public key is set
+    function _initialize(uint256 ownerKey, uint256 finishDate, uint128 ticketCost, uint8 percent){
+        l_ownerKey = ownerKey;
+        l_finishDate = finishDate;
+        l_ticketCost = ticketCost;
+        l_status = 0;
+        l_numTickets = 0;
+        l_percent = percent;
+        
+    }
+
+    constructor(uint256 ownerKey, uint256 finishDate, uint128 ticketCost, uint8 percent) public {
         require(tvm.pubkey() != 0, 101);
-        // Check that message has signature (msg.pubkey() is not zero) and
-        // message is signed with the owner's private key
         require(msg.pubkey() == tvm.pubkey(), 102);
-        // The current smart contract agrees to buy some gas to finish the
-        // current transaction. This actions required to process external
-        // messages, which bring no value (henceno gas) with themselves.
         tvm.accept();
         timestamp = now;
+        _initialize(ownerKey, finishDate, ticketCost, percent);
     }
 
     function buyTicket(adress payable t_owner) public {
-        uint tId = numTickets++;
+        uint tId = l_numTickets++;
         tickets[tId] = Ticket(t_owner);
     }
 
     function chooseWinner() public returns (uint256 winnerAddress) {
         uint num = 10;
-        Ticket t_winner = tickets(num);
+        Ticket t_winner = tickets.fetch(num);
         address payable dest = t_winner.t_owner;
-        address payable m_owner = adress(m_ownerKey);
+        address payable m_owner = adress(l_ownerKey);
         tvm.accept();
-        dest.transfer((numTickets * ticketCost) / 50, true, 0);
-        m_owner.transfer((numTickets * ticketCost) / 50, true, 0);
+        uint prize = ((l_numTickets *  l_ticketCost) / 100) * 50;
+        dest.transfer(prize , true, 0);
+        m_owner.transfer((l_numTickets * l_ticketCost) - prize, true, 0);
     }
+
+    function getNumOfSoldTickets() public returns (uint256 numOfSoldTickets){
+        numOfSoldTickets = l_numTickets;
+    }
+
+    function getTicket(uint64 tId) public view returns (Ticket ticket){
+        (bool exists, Ticket tf) = tickets.fetch(tId);
+        require(exists, 102);
+        ticket = tf;
+    }   
 
 }
